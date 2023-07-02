bb92db83c5729249764be18be64f4b5aae15bb03
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2023-07-02 21:21:43 +0530

    flexbox

--- App.js
+++ App.js
@@ -1,28 +1,32 @@
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.dummyText}>Another piece of text!</Text>
-      </View>
-      <Text style={styles.dummyText}>Hello World!</Text>
-      <Button title="Tap Me" />
-    </View>
-  );
+    return (
+        <View style={styles.appContainer}>
+            <View style={styles.inputContainer}>
+                <TextInput style={styles.textInput} placeholder="Your Course Goal!" />
+                <Button title="Add Goal" />
+            </View>
+            <View>
+                <Text>List of goals...</Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  dummyText: {
-    margin: 16,
-    padding: 16,
-    borderWidth: 2,
-    borderColor: "blue",
-  },
+    appContainer: {
+        padding: 50,
+    },
+    inputContainer: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: "#cccccc",
+        width: "80%",
+        marginRight: 8,
+        padding: 8,
+    },
 });
