71a8353bad0b50030afd53a004d68a40e73616b6
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2023-07-07 03:36:53 +0530

    managing list data

--- App.js
+++ App.js
@@ -3,13 +3,14 @@ import { useState } from "react";
 
 export default function App() {
     const [enteredGoal, setEnteredGoal] = useState("");
+    const [courseGoals, setCourseGoals] = useState([]);
 
     const goalInputHandler = (enteredText) => {
         setEnteredGoal(enteredText);
     };
 
     const addGoalHandler = () => {
-        console.log(enteredGoal);
+        setCourseGoals((currentGoals) => [...currentGoals, enteredGoal]);
     };
 
     return (
@@ -19,7 +20,9 @@ export default function App() {
                 <Button title="Add Goal" onPress={addGoalHandler} />
             </View>
             <View style={styles.goalsContainer}>
-                <Text>List of goals...</Text>
+                {courseGoals.map((goal) => (
+                    <Text key={goal}>{goal}</Text>
+                ))}
             </View>
         </View>
     );
