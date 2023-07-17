7204fde5733d78e8cf9d46d20aad3427d056c500
Adityaraj Sahu <adityaraj2019.sahu@gmail.com>
2023-07-17 16:17:17 +0530

    styling modal

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
-import { StyleSheet, View, FlatList } from "react-native";
+import { StyleSheet, View, FlatList, Button } from "react-native";
 import { useState } from "react";
 
 import GoalItem from "./components/GoalItem";
 import GoalInput from "./components/GoalInput";
 
 export default function App() {
+    const [modalIsVisible, setModalIsVisible] = useState(false);
     const [courseGoals, setCourseGoals] = useState([]);
+
+    const startAddGoalHandler = () => {
+        setModalIsVisible(true);
+    };
 
     const addGoalHandler = (enteredGoal) => {
         setCourseGoals((currentGoals) => [...currentGoals, { text: enteredGoal, id: Math.random().toString() }]);
@@ -19,7 +24,8 @@ export default function App() {
 
     return (
         <View style={styles.appContainer}>
-            <GoalInput onAddGoal={addGoalHandler} />
+            <Button title="Add New Goal" color="#5e0acc" onPress={startAddGoalHandler} />
+            <GoalInput visible={modalIsVisible} onAddGoal={addGoalHandler} />
             <View style={styles.goalsContainer}>
                 <FlatList
                     data={courseGoals}
